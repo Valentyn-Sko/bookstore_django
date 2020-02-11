7a58a09956f840fcb8ac5aa5240a4a7f49721f2a
Valentyn Skorych <vskorych@spscommerce.com>
2020-02-11 18:42:07 +0200

    added registration templates

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registrations',
     'store',
 ]
 
@@ -119,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# registration
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
--- bookstore/bookstore/urls.py
+++ bookstore/bookstore/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('registration,backend.default.urls')),
     path('store/', include('store.urls'), name='store'),
 ]
